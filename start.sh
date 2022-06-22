8c6ba72d31303390e69b6aa1f249efbe845f13e8
Decoderseeman <99487702+Decoderseeman@users.noreply.github.com>
2022-06-22 13:45:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Decoderseeman/Param-Sundari-for-MKV-BLASTERS.git /Ajax
+  git clone https://github.com/Decoderseeman/Param-Sundari-for-MKV-BLASTERS.git /Param-Sundari-for-MKV-BLASTERS
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
